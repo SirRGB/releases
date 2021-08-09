9da5fcbe83e7c43fe1dc93a46046f3480ced90af
SirRGB <liam_wagner@gmx.de>
2021-08-09 22:26:56 +0200

    bringup arrowos cedric

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device=""
+export device="cedric"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Arrow OS"
+export ROM_DIR="${WORKSPACE}/../Arr11"
+export ROM_VERSION="11"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A11Arrow.xml"
+export manifest_url="https://github.com/ArrowOS-Revived/android_manifest.git"
+export rom_vendor_name="arrow"
+export branch="arrow-11.0"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="user"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="SirRGB/releases"
 
 export timezone="UTC"
