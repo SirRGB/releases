04af1d34418003da0da1bbb4f31cdf9d5951d38c
Bedirhan KURT <windowz414@gnuweeb.org>
2021-11-27 11:50:43 +0300

    Add plenty of features
    * Introducing messages with images embedded for Telegram chat
      announcements! telegram.sh version 0.3 introduced support for
      text stylings for all message types.
      Commit: https://github.com/fabianonline/telegram.sh/commit/707335ac53778903cf030a152ff640a81815749d
      (set parse mode for all types of messages)
    
      Added this feature as I tested that it won't cause any problems
      with messages sent.
      Tested-chat: https://t.me/owZ_Builds/343
      Note: The first 2 builds testing this were unable to locate build
      fail image. (build.sh was sourcing config.sh again, which resets
      RELEASES_DIR variable. Hardcoded redeclaration of RELEASES_DIR
      correctly by echoing output of `cd -` for now. Future versions
      of bash/zsh might break it. This behavior will change in a future
      commit.)
    
    Signed-off-by: Bedirhan KURT <windowz414@gnuweeb.org>

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Sync started for ${manifest_url}/tree/${branch}"
 if [ "${jenkins}" == "true" ]; then
-    telegram -M "Sync started for [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch}): [See Progress](${BUILD_URL}console)"
+    telegram -i ${RELEASES_DIR}/assets/sync1.png -M "Sync started for [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch}): [See Progress](${BUILD_URL}console)"
 else
-    telegram -M "Sync started for [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch})"
+    telegram -i ${RELEASES_DIR}/assets/sync1.png -M "Sync started for [${ROM} ${ROM_VERSION}](${manifest_url}/tree/${branch})"
 fi
 SYNC_START=$(date +"%s")
 if [ "${official}" != "true" ]; then
@@ -11,7 +11,14 @@ if [ "${official}" != "true" ]; then
     mkdir -p .repo/local_manifests
     wget "${local_manifest_url}" -O .repo/local_manifests/manifest.xml
 fi
-repo init -u "${manifest_url}" -b "${branch}" --depth 1
+if [ -z ${referencedir} ]; then
+    echo "referencedir is not set. Sync will happen completely dependant on your Internet connection."
+    repo init -u "${manifest_url}" -b "${branch}" --depth 1
+else
+    echo "Taking ${referencedir} as reference for existing checkouts."
+    echo "Be aware that manifests using different configuration style (e.g. lineage on reference and laos on current for LineageOS) won't take each other as reference!"
+    repo init -u "${manifest_url}" -b "${branch}" --depth 1 --reference "${referencedir}"
+fi
 cores=$(nproc --all)
 if [ "${cores}" -gt "8" ]; then
     cores=8
@@ -22,11 +29,11 @@ SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
 if [ "${syncsuccessful}" == "0" ]; then
     echo "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
-    telegram -N -M "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -i ${RELEASES_DIR}/assets/sync3.png -N -M "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
     source "${my_dir}/build.sh"
 else
     echo "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
-    telegram -N -M "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -i ${RELEASES_DIR}/assets/sync2.png -N -M "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
     curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
     exit 1
 fi
