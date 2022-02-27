9b8bc0dfad7a723bfbf6cbbfeb75c2361aadda35
SirRGB <liam_wagner@gmx.de>
2022-02-27 15:51:17 +0100

    add crdroid for cheese

--- config.sh
+++ config.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@amyrom.ml"
 
-export device=""
+export device="cheeseburger"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="crDroid"
+export ROM_DIR="${WORKSPACE}/../Crd11"
+export ROM_VERSION="7.15"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cheeseburgerdumpling/A11Crdroid3.xml"
+export manifest_url="https://github.com/crdroidandroid/android"
+export rom_vendor_name="lineage"
+export branch="11.0"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="user"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="SirRGB/releases"
 
 export timezone="UTC"
