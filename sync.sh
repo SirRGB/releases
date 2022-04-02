3b07340eb489139065cee99bec055f1659ffa5d1
SirRGB <liam_wagner@gmx.de>
2022-04-02 22:10:48 +0200

    add carbonROM for cedric

--- sync.sh
+++ sync.sh
@@ -10,6 +10,7 @@ rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
 wget "${local_manifest_url}" -O .repo/local_manifests/manifest.xml
 repo init -u "${manifest_url}" -b "${branch}" --depth 1
+repo init -u "${manifest_url}" -b "${branch}" --depth 1 --reference "${WORKSPACE}/../Min10"
 cores=$(nproc --all)
 if [ "${cores}" -gt "8" ]; then
     cores=8
