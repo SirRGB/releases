2789ac19f318f3f3a6a201213dd3146044e158a4
SirRGB <sirrgb@amyrom.ml>
2022-04-30 15:59:18 +0200

    add twrp for cedric

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@amyrom.ml"
 
-export device=""
+export device="cedric"
 
-export ROM_DIR=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM_DIR="${WORKSPACE}/TWRP7"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A7.1Twrp.xml"
+export manifest_url="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
+export rom_vendor_name="omni"
+export branch="twrp-7.1"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
-export jenkins="false"
+export jenkins="true"
 
 export release_repo=""
 
