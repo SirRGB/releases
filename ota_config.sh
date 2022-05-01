98fd4304fed5406d13eb293a905227e4840be817
SirRGB <sirrgb@amyrom.ml>
2022-05-01 13:10:50 +0200

    fix ota output

--- ota_config.sh
+++ ota_config.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-mkdir "${WORKSPACE}/../ota"
-cd "${WORKSPACE}/../ota"
-rm -rf ota_config
-cd ota_config
+echo ""
 echo ""
 echo -n '{"response":['> "${device}.json"
 echo -n {\"datetime\": \"`basename ${BUILD_START}`\",\"filename\": \"`basename ${finalzip_path}`\",\"id\": \"`sha256sum ${finalzip_path} | awk '{ print $1 }'`\",\"romtype\": \"unofficial\",\"size\": `stat -c%s ${finalzip_path}`,\"url\": \"https://github.com/${release_repo}/releases/download/${tag}/${zip_name}\",\"version\": \"16.0\"} >> '${device}.json'
 echo ']}'>>"${device}.json"
 echo ""
+echo ""
+cat '${device}.json'
