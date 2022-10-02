1f1e9466ede5c1267cef801546c91a9d4871204e
SirRGB <sirrgb@proton.me>
2022-10-02 03:16:06 +0200

    adapt to a5y17lte
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device=""
+export device="a5y17lte"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="Lineage OS"
+export ROM_DIR="${WORKSPACE}/../Los12"
+export ROM_VERSION="19.1"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/a57y17lte/A12Lineage.xml"
+export manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage"
+export branch="lineage-19.1"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="yes"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="SirRGB/releases"
 
 export timezone="UTC"
