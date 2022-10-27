b944ac63ca3361207d795b958ad2cbcb08c2b939
SirRGB <sirrgb@proton.me>
2022-10-27 22:59:54 +0200

    overwrite instead of append ota info

--- ota_config.sh
+++ ota_config.sh
@@ -3,7 +3,7 @@ echo ""
 echo ""
 echo -n '{"response":['> "${device}.json"
 echo -n {\"datetime\": \"`basename ${BUILD_START}`\",\"filename\": \"`basename ${finalzip_path}`\",\"id\": \"`sha256sum ${finalzip_path} | awk '{ print $1 }'`\",\"romtype\": \"unofficial\",\"size\": `stat -c%s ${finalzip_path}`,\"url\": \"https://github.com/${release_repo}/releases/download/${tag}/${zip_name}\",\"version\": \"8.0\"} >> '${device}.json'
-echo ']}'>>"${device}.json"
+echo ']}'>"${device}.json"
 echo ""
 echo ""
 cat '${device}.json'
