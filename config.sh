4ba68c4e301fe5c7811c7bb5ee183c454c9d924d
SirRGB <sirrgb@proton.me>
2022-12-12 15:57:54 +0100

    adapt to a5y17lte

--- config.sh
+++ config.sh
@@ -3,12 +3,12 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="cedric"
+export device="a5y17lte"
 
 export ROM="Lineage OS"
 export ROM_DIR="${WORKSPACE}/../Los12"
 export ROM_VERSION="19.1"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A12Lineage.xml"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/a57y17lte/A12Lineage.xml"
 export manifest_url="https://github.com/LineageOS/android"
 export rom_vendor_name="lineage"
 export branch="lineage-19.1"
