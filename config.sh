416d59cfdc359786721818a1851e134b785cb481
SirRGB <sirrgb@proton.me>
2022-12-23 17:38:57 +0100

    use custom trees

--- config.sh
+++ config.sh
@@ -6,10 +6,11 @@ export GITHUB_EMAIL="sirrgb@amyrom.ml"
 export device="cedric"
 
 export ROM_DIR="${WORKSPACE}/../TWRP7"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A7.1Twrp.xml"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A7.1Twrp2.xml"
 export manifest_url="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
 export rom_vendor_name="omni"
 export branch="twrp-7.1"
+export WITH_KERNEL_SOURCE="true"
 
 export ccache="false"
 export ccache_size=""
