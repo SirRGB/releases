7a0359f99d40c634f5050b991c0bb4b925a9610f
SirRGB <sirrgb@proton.me>
2022-12-28 00:25:40 +0100

    pdx203: bringup

--- config.sh
+++ config.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 export GITHUB_USER="SirRGB"
-export GITHUB_EMAIL="sirrgb@amyrom.ml"
+export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="cedric"
+export device="pdx203"
 
-export ROM_DIR="${WORKSPACE}/../TWRP7"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A7.1Twrp2.xml"
-export manifest_url="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
-export rom_vendor_name="omni"
-export branch="twrp-7.1"
-export WITH_KERNEL_SOURCE="true"
+export ROM_DIR="${WORKSPACE}/../TWRP12"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/pdx203/A12Twrp.xml"
+export manifest_url="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
+export rom_vendor_name="twrp"
+export branch="twrp-12.1"
 
 export ccache="false"
 export ccache_size=""
 
-export jenkins="true"
+export jenkins="false"
 
 export release_repo="SirRGB/releases"
 
