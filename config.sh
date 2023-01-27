db1180404e8d4ce8c3fdeba9d7f9a750f50053a6
SirRGB <sirrgb@proton.me>
2023-01-27 16:23:02 +0100

    bringup rr griffin

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device=""
+export device="griffin"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="RR-Rev"
+export ROM_DIR="${WORKSPACE}/../RR10"
+export ROM_VERSION="Q"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A10RR.xml"
+export manifest_url="https://github.com/ResurrectionRemix-Revived/manifest.git"
+export rom_vendor_name="rr"
+export branch="Q"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="user"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="true"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="SirRGB/releases"
 
 export timezone="UTC"
