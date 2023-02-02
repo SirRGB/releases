f027dab6544998478d93242faa807bfba00a1e45
SirRGB <sirrgb@proton.me>
2023-02-02 16:33:17 +0100

    reborn!

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 export GITHUB_USER="SirRGB"
-export GITHUB_EMAIL="sirrgb@amyrom.ml"
+export GITHUB_EMAIL="sirrgb@proton.me"
 
 export device="griffin"
 
 export ROM="amyROM"
 export ROM_DIR="${WORKSPACE}/../Amy11"
-export ROM_VERSION="LTS"
-export official="true"
-export local_manifest_url=""
+export ROM_VERSION="4"
+export official="false"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A11Lineage2.xml"
 export manifest_url="https://github.com/amyROM/manifest"
-export rom_vendor_name="amy"
-export branch="rosie-lts"
+export rom_vendor_name="lineage"
+export branch="reborn"
 export bacon="bacon"
 export buildtype="user"
 export clean="installclean"
@@ -22,7 +22,7 @@ export upload_recovery="false"
 export ccache="false"
 export ccache_size=""
 
-export jenkins="true"
+export jenkins="false"
 
 export release_repo="SirRGB/releases"
 
