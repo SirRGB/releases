c18f8389fcf865d03f03fc3f172a884125f3e3d2
SirRGB <sirrgb@proton.me>
2023-03-10 12:07:34 +0100

    adapt to dump
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="cheeseburger"
+export device="dumpling"
 
 export ROM="crDroid"
 export ROM_DIR="${WORKSPACE}/../Crd13"
