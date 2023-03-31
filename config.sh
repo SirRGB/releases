fe43a72f9a18fac7a033346081a6c9f0255f408a
SirRGB <sirrgb@proton.me>
2023-03-31 17:13:10 +0200

    adapt to a5y17lte

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="griffin"
+export device="a5y17lte"
 
 export ROM="Lineage OS"
 export ROM_DIR="${WORKSPACE}/../Los10"
 export ROM_VERSION="17.1"
 export official="false"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A10Lineage2.xml"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/a57y17lte/A10Lineage.xml"
 export manifest_url="https://github.com/LineageOS-Revived/android"
 export rom_vendor_name="lineage"
 export branch="lineage-17.1"
