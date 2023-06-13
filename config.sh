7d11c262cb7f9fad10150fcc199c8a35f83b9961
SirRGB <sirrgb@proton.me>
2023-06-13 19:24:31 +0200

    adapt to cipher

--- config.sh
+++ config.sh
@@ -5,13 +5,13 @@ export GITHUB_EMAIL="sirrgb@proton.me"
 
 export device="griffin"
 
-export ROM="Sakura Revived"
-export ROM_DIR="${WORKSPACE}/../Sak10"
-export ROM_VERSION="10"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A10Sakura.xml"
-export manifest_url="https://github.com/Sakura-Revived/android"
+export ROM="CipherOS Revived"
+export ROM_DIR="${WORKSPACE}/../Cip11"
+export ROM_VERSION="1.8"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A11Cipher.xml"
+export manifest_url="https://github.com/SirRGB/android_manifest"
 export rom_vendor_name="lineage"
-export branch="10"
+export branch="eleven"
 export bacon="bacon"
 export buildtype="user"
 export clean="installclean"
@@ -21,7 +21,7 @@ export upload_recovery="false"
 export ccache="false"
 export ccache_size=""
 
-export jenkins="true"
+export jenkins="false"
 
 export release_repo="SirRGB/releases"
 
