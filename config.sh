2e94846f499ac63f525396747611c35098a010cc
SirRGB <sirrgb@proton.me>
2023-06-13 19:25:30 +0200

    adapt to cedric

--- config.sh
+++ config.sh
@@ -3,12 +3,12 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="griffin"
+export device="cedric"
 
 export ROM="CipherOS Revived"
 export ROM_DIR="${WORKSPACE}/../Cip11"
 export ROM_VERSION="1.8"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A11Cipher.xml"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A11Cipher.xml"
 export manifest_url="https://github.com/SirRGB/android_manifest"
 export rom_vendor_name="lineage"
 export branch="eleven"
