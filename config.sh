f9ad2eff10f94237217abd675aeceab9e4603e6d
SirRGB <sirrgb@proton.me>
2023-12-03 01:10:41 +0100

    adapt for akatsuki
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="akari"
+export device="akatsuki"
 
 export ROM="crDroid"
 export ROM_DIR="${WORKSPACE}/../Crd13"
