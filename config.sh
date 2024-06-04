3fab8566690a89142e4381c8eda369c7eb324a2b
SirRGB <sirrgb@proton.me>
2024-06-04 19:13:22 +0200

    adapt to ali

--- config.sh
+++ config.sh
@@ -3,12 +3,12 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="griffin"
+export device="ali"
 
 export ROM="Lineage OS"
 export ROM_DIR="${WORKSPACE}/../Los12"
 export ROM_VERSION="19.1"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/griffin/A12Lineage.xml"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/ali/A12Lineage2.xml"
 export manifest_url="https://github.com/droid-legacy/android"
 export rom_vendor_name="lineage"
 export branch="lineage-19.1"
