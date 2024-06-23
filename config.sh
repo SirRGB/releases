0d2ddf63ead11ec501c07d36bbcad55db1598101
SirRGB <sirrgb@proton.me>
2024-06-23 21:14:35 +0200

    adapt to ali
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config.sh
+++ config.sh
@@ -3,12 +3,12 @@
 export GITHUB_USER="SirRGB"
 export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device="cedric"
+export device="ali"
 
 export ROM="Lineage OS"
 export ROM_DIR="${WORKSPACE}/../Los13"
 export ROM_VERSION="20"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A13Lineage.xml"
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/ali/A13Lineage.xml"
 export manifest_url="https://github.com/droid-legacy/android"
 export rom_vendor_name="lineage"
 export branch="lineage-20.0"
