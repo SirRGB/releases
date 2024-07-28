51adf36ccef760f5ffdc160fd30a6b7bcb8f4a36
SirRGB <sirrgb@proton.me>
2024-07-28 11:05:35 +0200

    capitalize release tag
    * taken from ro.lineage.releasetype as per docs
    
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- ota_config.sh
+++ ota_config.sh
@@ -3,7 +3,7 @@ cd ${ROM_DIR}_ota/
 echo ""
 echo ""
 echo -n '{"response":['>${device}.json
-echo -n {\"datetime\": \"`basename ${BUILD_START}`\",\"filename\": \"`basename ${finalzip_path}`\",\"id\": \"`sha256sum ${finalzip_path} | awk '{ print $1 }'`\",\"romtype\": \"unofficial\",\"size\": `stat -c%s ${finalzip_path}`,\"url\": \"https://github.com/${release_repo}/releases/download/${tag}/${zip_name}\",\"version\": \"${ROM_VERSION}\"}>>${device}.json
+echo -n {\"datetime\": \"`basename ${BUILD_START}`\",\"filename\": \"`basename ${finalzip_path}`\",\"id\": \"`sha256sum ${finalzip_path} | awk '{ print $1 }'`\",\"romtype\": \"UNOFFICIAL\",\"size\": `stat -c%s ${finalzip_path}`,\"url\": \"https://github.com/${release_repo}/releases/download/${tag}/${zip_name}\",\"version\": \"${ROM_VERSION}\"}>>${device}.json
 echo ']}'>>${device}.json
 cat ${device}.json
 echo ""
